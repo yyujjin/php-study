e427fe2db4f8e59692f62c7966a611236b1bf78d
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-01-29 16:43:40 +0900

    ✨ 총 주 수 계산 오류 수정 및 get 방식으로 이전/이후 날짜 변경 로직 추가

--- calendar/index.php
+++ calendar/index.php
@@ -1,11 +1,11 @@
 <?php
-$year = date('Y');
-$month = date('m'); 
+$year = $_GET['year']??date('Y');
+$month = $_GET['month']??date('m');
 ?>
 
-<a>이전 달</a>
-<span>2025년 1월</span>
-<a>다음 달</a>
+<a href="<?=prevButton($year,$month)?>">이전 달</a>
+<span><?=$year?>년 <?=$month?>월</span>
+<a href="<?=nextButton($year,$month)?>">다음 달</a>
 <a>오늘로 돌아가기</a>
 
 <table border="1">
@@ -23,14 +23,31 @@ $month = date('m');
     </tr>
 </table>
 
-
-
 <?php
-function createDateCells($year,$month){
 
+
+//이후
+function nextButton($year,$month){
+    if($month==12){
+        return "?year=".($year+1)."&month=1";
+    }
+    return "?year=".$year."&month=".($month+1);
+}
+
+//이전
+function prevButton($year,$month){
+    if($month==1){
+        return "?year=".($year-1)."&month=12";
+    }
+    return "?year=".$year."&month=".($month-1);
+}
+
+
+//달력 칸 만드는 함수
+function createDateCells($year,$month){
     $lastDay = date("t", strtotime("$year-$month-01"));
-    $totalWeek = ceil($lastDay/7);
     $startMonth = date("w", strtotime("$year-$month-01"));
+    $totalWeek = ceil(($lastDay+$startMonth)/7);
     $day=1;
     for($i=0; $i<$totalWeek; $i++){ 
         echo "<tr>";

84e1b733fec0d9636844c26eb4bf6061aa92af88
yyujjin <141253669+yyujjin@users.noreply.github.com>
2025-02-11 11:19:44 +0900

    ✨ 조건에 맞게 스타일 적용하고 일정 출력 로직 생성

--- jiwon/remake2.php
+++ jiwon/remake2.php
@@ -5,7 +5,6 @@ $planArr = [
     ['start' => '2024-12-15', 'end' => '2025-06-15', 'label' => '일정1', 'repeatType' => 'week', 'repeatData' => '수'],
     ['start' => '2025-02-03', 'end' => '2025-02-20', 'label' => '일정2', 'repeatType' => 'week', 'repeatData' => '목'],
     ['start' => '2023-09-15', 'end' => '2023-09-20', 'label' => '일정3', 'repeatType' => 'week', 'repeatData' => '금'],
-    ['start' => '2023-09-10', 'end' => '2023-09-20', 'label' => '일정4', 'repeatType' => 'week', 'repeatData' => '월'],
 ];
 
 //시작일 기준으로 오름차순 정렬
@@ -50,7 +49,63 @@ foreach ($planArr as $plan) {
                     if ($i == 0 && $j < $startDate) {
                         echo "<td></td>";
                     } elseif ($day <= $lastDay) {
-                        echo "<td>$day</td>";
+
+                        $currentDate = date('w', strtotime("$year-$month-$day")); //요일
+
+                        //여기에 들어오는 날이 조건에 해당하는지
+                        //그걸 확인하기 위해서 오늘형식으로 바꾸는데 month는 한자리로 바꾸기
+                        $month = $startDay->format('n');
+                        $currentDay = "$year-$month-$day"; //지금 날짜
+
+                        //일정이 중복되는지 확인
+                        $duplicationCount = 0; //1이상이면 중복, 1이면 한번
+
+                        //중복에 맞게 배경색 다르게 하기
+                        $bgColor = "";
+
+                        //해당하는 라벨 다 모으기
+                        $labels = [];
+
+                        //반복 요일인지 확인하고 굵게 처리
+                        $isBold = "";
+
+                        foreach ($planArr as $plan) {
+                            //기간에 해당하는지
+                            if (strtotime($currentDay) >= strtotime($plan['start']) && strtotime($currentDay) <= strtotime($plan['end'])) {
+                                $duplicationCount++;
+                                $labels[] = $plan['label'];
+
+                                //중복 요일확인하기
+                                //지금 들어온 요일이 plan의 요일에 해당되는게 있다면 굵게
+                                if ($plan['repeatType'] == 'week') {
+                                    //요일 숫자로바꾸기 
+                                    $convertNumTheDate = array_search($plan['repeatData'], ['일', '월', '화', '수', '목', '금', '토']);
+
+                                    //중복되는게 있다면 굵게 바꾸기
+                                    if ($convertNumTheDate == $currentDate) {
+                                        $isBold = 'font-weight: bold;';
+                                    }
+                                }
+                            }
+                        }
+
+                        //스타일 입히기
+                        if ($duplicationCount > 1) { //중복
+                            $bgColor = 'background-color: yellow;';
+                        }
+                        if ($duplicationCount == 1) { //한번
+                            $bgColor = 'background-color: red;';
+                        }
+
+                        //스타일 적용
+                        $style = 'style = "' . $bgColor . ' ' . $isBold . '"';
+
+                        //라벨 출력
+                        $label = implode("<br>", $labels);
+
+
+                        echo "<td " . $style . ">$day<br>$label</td>";
+
                         $day++;
                     } else {
                         echo "<td></td>";
